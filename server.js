02024487e2b388e315a5949ce82a05a68600bdfb
Agung Tuah Ananda <anan474@myself.com>
2020-10-27 14:23:27 +0700

    env example

--- server.js
+++ server.js
@@ -9,21 +9,21 @@ const app = express();
 
 const path = require("path");
 
-// Setup express server port from ENV, default: 3000
+// port yang digunakan server express, diatur di ENV, default: 3000
 app.set("port", process.env.PORT || 3000);
 
-// Enable only in development HTTP request logger middleware
+// hanya nyalakan ketika develop
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-// for parsing json
+// parsing json
 app.use(
   bodyParser.json({
     limit: "20mb",
   })
 );
-// for parsing application/x-www-form-urlencoded
+// parsing application/x-www-form-urlencoded
 app.use(
   bodyParser.urlencoded({
     limit: "20mb",
@@ -31,7 +31,6 @@ app.use(
   })
 );
 
-// Init all other stuff
 app.use(cors());
 app.use(compression());
 app.use(helmet());
